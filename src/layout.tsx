1b79bf5cf33c341206f063893b6920d6f45648dd
jasleen kaur <jjasleenkaur2@rrc.ca>
2026-06-06 15:20:54 -0500

    add code for clerk in the layout.tsx

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -19,15 +20,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}:{
   children: React.ReactNode;
-}>) {
+}) {
   return (
+    <ClerkProvider>
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
